f7a651862772b2815d824e02d4d5a76ce0d46d9c
pietra202 <pipispietragodoifelipe@gmail.com>
2024-08-27 17:00:11 -0300

    finalização aplicação MVC linq

---
+++ AplicacaoMCVlinq/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using AplicacaoMVCLinq.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacaoMVCLinq.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetClientes()
+        {
+            List<Clientes> listaClientes = new List<Clientes>();
+
+            Clientes c1 = new Clientes(1, "Alice", "alice@example.com", "1234-5678", new DateOnly(2000, 6, 1));
+            listaClientes.Add(c1);
+            Clientes c2 = new Clientes(2, "Bob", "bob@example.com", "2345-6789", new DateOnly(2001, 10, 06));
+            listaClientes.Add(c2);
+            Clientes c3 = new Clientes(3, "Charlie", "charlie@example.com", "3456-7890", new DateOnly(1999, 01, 30));
+            listaClientes.Add(c3);
+            Clientes c4 = new Clientes(4, "Eve", "eve@example.com", "3456-7890", new DateOnly(1999, 01, 30));
+            listaClientes.Add(c4);
+            Clientes c5 = new Clientes(5, "David", "david@example.com", "4567-8901", new DateOnly(2000, 12, 15));
+            listaClientes.Add(c5);
+
+            return View(listaClientes);
+        }
+    }
+}
---
+++ AplicacaoMCVlinq/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿namespace AplicacaoMVCLinq.Models
+{
+    public class Clientes
+    {
+        public int ClientesId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateOnly DataNasc { get; set; }
+
+        public Clientes(int id, string nome, string email, string telefone, DateOnly dataNasc)
+        {
+            this.ClientesId = id;
+            this.Nome = nome;
+            this.Email = email;
+            this.Telefone = telefone;
+            this.DataNasc = dataNasc;
+        }
+    }
+}
